5d31f2f8d32927b74a2e0f0842f1314f5cf71865
Auxilor <will@willfp.com>
2020-11-03 18:31:32 +0000

    Added support for 1.16.4

---
+++ NMS/v1_16_R3/src/main/java/com/willfp/ecoenchants/v1_16_R3/BlockBreak.java
@@ -0,0 +1,14 @@
+package com.willfp.ecoenchants.v1_16_R3;
+
+import com.willfp.ecoenchants.nms.API.BlockBreakWrapper;
+import net.minecraft.server.v1_16_R2.BlockPosition;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class BlockBreak implements BlockBreakWrapper {
+    @Override
+    public void breakBlock(Player player, Block block) {
+        ((CraftPlayer)player).getHandle().playerInteractManager.breakBlock(new BlockPosition(block.getX(), block.getY(), block.getZ()));
+    }
+}
---
+++ NMS/v1_16_R3/src/main/java/com/willfp/ecoenchants/v1_16_R3/Cooldown.java
@@ -0,0 +1,11 @@
+package com.willfp.ecoenchants.v1_16_R3;
+
+import com.willfp.ecoenchants.nms.API.CooldownWrapper;
+import org.bukkit.entity.Player;
+
+public class Cooldown implements CooldownWrapper {
+    @Override
+    public double getAttackCooldown(Player player) {
+        return player.getAttackCooldown();
+    }
+}
