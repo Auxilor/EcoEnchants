73fc48c693ff2c898f7c32347753bafa2fbbeb8d
Auxilor <william.favierparsons1@gmail.com>
2020-11-19 09:07:40 +0000

    Optimised AnvilGUI integration

--- Plugin/src/main/java/com/willfp/ecoenchants/integrations/anvilgui/plugins/AnvilGUIImpl.java
+++ Plugin/src/main/java/com/willfp/ecoenchants/integrations/anvilgui/plugins/AnvilGUIImpl.java
@@ -4,18 +4,24 @@ import com.willfp.ecoenchants.EcoEnchantsPlugin;
 import com.willfp.ecoenchants.integrations.anvilgui.AnvilGUIIntegration;
 
 public class AnvilGUIImpl implements AnvilGUIIntegration {
+    private static Class<?> ANVIL_GUI_CLASS = null;
+
     @Override
     public boolean isInstance(Object object) {
-        try {
-            return object.getClass().equals(Class.forName("net.wesjd.anvilgui.version.Wrapper" + EcoEnchantsPlugin.NMS_VERSION.substring(1) + "$AnvilContainer"));
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return false;
-        }
+        assert ANVIL_GUI_CLASS != null;
+        return object.getClass().equals(ANVIL_GUI_CLASS);
     }
 
     @Override
     public String getPluginName() {
         return "AnvilGUI";
+    }
+
+    static {
+        try {
+            ANVIL_GUI_CLASS = Class.forName("net.wesjd.anvilgui.version.Wrapper" + EcoEnchantsPlugin.NMS_VERSION.substring(1) + "$AnvilContainer");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
