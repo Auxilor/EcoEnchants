08e387238c9e37c61eb257506b28a01fcbeab4cd
Auxilor <william.favierparsons1@gmail.com>
2020-11-27 13:00:03 +0000

    Finished refactoring update checker

--- Plugin/src/main/java/com/willfp/ecoenchants/listeners/PlayerJoinListener.java
+++ Plugin/src/main/java/com/willfp/ecoenchants/listeners/PlayerJoinListener.java
@@ -2,6 +2,7 @@ package com.willfp.ecoenchants.listeners;
 
 import com.willfp.ecoenchants.EcoEnchantsPlugin;
 import com.willfp.ecoenchants.config.ConfigManager;
+import com.willfp.ecoenchants.util.UpdateChecker;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,10 +10,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if (EcoEnchantsPlugin.outdated) {
+        if (UpdateChecker.isOutdated()) {
             if (event.getPlayer().hasPermission("ecoenchants.updateannounce")) {
                 event.getPlayer().sendMessage(ConfigManager.getLang().getMessage("outdated").replace("%ver%", EcoEnchantsPlugin.getInstance().getDescription().getVersion())
-                        .replace("%newver%", EcoEnchantsPlugin.newVersion));
+                        .replace("%newver%", UpdateChecker.getNewVersion()));
             }
         }
     }
