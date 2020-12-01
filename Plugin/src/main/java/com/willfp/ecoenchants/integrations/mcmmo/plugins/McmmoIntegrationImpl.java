9b10bdc51ce81aa43570f56d8f4631bcbfd0fab4
Auxilor <william.favierparsons1@gmail.com>
2020-12-01 14:58:23 +0000

    Fixed FakeEvent noClassDefFoundException on mcmmo classic

--- Plugin/src/main/java/com/willfp/ecoenchants/integrations/mcmmo/plugins/McmmoIntegrationImpl.java
+++ Plugin/src/main/java/com/willfp/ecoenchants/integrations/mcmmo/plugins/McmmoIntegrationImpl.java
@@ -2,6 +2,7 @@ package com.willfp.ecoenchants.integrations.mcmmo.plugins;
 
 import com.gmail.nossr50.events.fake.FakeEvent;
 import com.willfp.ecoenchants.integrations.mcmmo.McmmoIntegration;
+import com.willfp.ecoenchants.util.ClassUtils;
 import org.bukkit.event.Event;
 
 /**
@@ -10,6 +11,8 @@ import org.bukkit.event.Event;
 public class McmmoIntegrationImpl implements McmmoIntegration {
     @Override
     public boolean isFake(Event event) {
+        if(!ClassUtils.exists("com.gmail.nossr50.events.fake.FakeEvent"))
+            return false;
         return event instanceof FakeEvent;
     }
 
