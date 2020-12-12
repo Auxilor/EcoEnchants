00960a22787bb35f7a55a2cff77f128cc36f59dd
Auxilor <william.favierparsons1@gmail.com>
2020-12-12 12:31:31 +0000

    Removed unused config-versions

--- Plugin/src/main/java/com/willfp/ecoenchants/config/ConfigManager.java
+++ Plugin/src/main/java/com/willfp/ecoenchants/config/ConfigManager.java
@@ -1,21 +1,11 @@
 package com.willfp.ecoenchants.config;
 
-import com.willfp.ecoenchants.config.configs.Config;
-import com.willfp.ecoenchants.config.configs.EnchantmentConfig;
-import com.willfp.ecoenchants.config.configs.Lang;
-import com.willfp.ecoenchants.config.configs.Rarity;
-import com.willfp.ecoenchants.config.configs.Target;
+import com.willfp.ecoenchants.config.configs.*;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ConfigManager {
-    public static final HashMap<String, Double> configVersions = new HashMap<String, Double>() {{
-        put("target", 1.0);
-        put("rarity", 1.0);
-    }};
-
     private static final Lang LANG = new Lang();
     private static final Config CONFIG = new Config();
     private static final Target TARGET = new Target();
