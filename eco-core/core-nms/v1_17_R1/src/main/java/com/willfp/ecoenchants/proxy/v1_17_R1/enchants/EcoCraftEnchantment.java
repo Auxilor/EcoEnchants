bde29c91204dbfff36a9ffd2eb36897c52d310d2
Auxilor <william.favierparsons1@gmail.com>
2021-06-11 12:24:24 +0100

    Added 1.17 support and updated to Java 16

---
+++ eco-core/core-nms/v1_17_R1/src/main/java/com/willfp/ecoenchants/proxy/v1_17_R1/enchants/EcoCraftEnchantment.java
@@ -0,0 +1,61 @@
+package com.willfp.ecoenchants.proxy.v1_17_R1.enchants;
+
+import com.willfp.ecoenchants.enchantments.EcoEnchants;
+import com.willfp.ecoenchants.enchantments.support.vanilla.VanillaEnchantmentMetadata;
+import net.minecraft.world.item.enchantment.Enchantment;
+import org.bukkit.NamespacedKey;
+import org.bukkit.craftbukkit.v1_17_R1.enchantments.CraftEnchantment;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("unchecked")
+public class EcoCraftEnchantment extends CraftEnchantment {
+    private final VanillaEnchantmentMetadata metadata;
+
+    public EcoCraftEnchantment(@NotNull final Enchantment target,
+                               @NotNull final VanillaEnchantmentMetadata metadata) {
+        super(target);
+        this.metadata = metadata;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return metadata.getMaxLevel() == null ? this.getHandle().getMaxLevel() : metadata.getMaxLevel();
+    }
+
+    @Override
+    public boolean conflictsWith(@NotNull final org.bukkit.enchantments.Enchantment other) {
+        return EcoEnchants.getFromEnchantment(other) == null ? super.conflictsWith(other) : other.conflictsWith(other);
+    }
+
+    public void register() {
+        try {
+            Field byIdField = org.bukkit.enchantments.Enchantment.class.getDeclaredField("byKey");
+            Field byNameField = org.bukkit.enchantments.Enchantment.class.getDeclaredField("byName");
+            byIdField.setAccessible(true);
+            byNameField.setAccessible(true);
+            Map<NamespacedKey, org.bukkit.enchantments.Enchantment> byKey = (Map<NamespacedKey, org.bukkit.enchantments.Enchantment>) byIdField.get(null);
+            Map<String, org.bukkit.enchantments.Enchantment> byName = (Map<String, org.bukkit.enchantments.Enchantment>) byNameField.get(null);
+            byKey.remove(this.getKey());
+            byName.remove(this.getName());
+
+            Map<String, org.bukkit.enchantments.Enchantment> byNameClone = new HashMap<>(byName);
+            for (Map.Entry<String, org.bukkit.enchantments.Enchantment> entry : byNameClone.entrySet()) {
+                if (entry.getValue().getKey().equals(this.getKey())) {
+                    byName.remove(entry.getKey());
+                }
+            }
+
+            Field f = org.bukkit.enchantments.Enchantment.class.getDeclaredField("acceptingNew");
+            f.setAccessible(true);
+            f.set(null, true);
+            f.setAccessible(false);
+
+            org.bukkit.enchantments.Enchantment.registerEnchantment(this);
+        } catch (NoSuchFieldException | IllegalAccessException ignored) {
+        }
+    }
+}
