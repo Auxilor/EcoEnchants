47ea2974684c34b7562830995c553d899291587f
Auxilor <william.favierparsons1@gmail.com>
2021-06-13 10:54:24 +0100

    Added 1.17 particles

---
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/ecoenchants/artifact/SkulkArtifact.java
@@ -0,0 +1,20 @@
+package com.willfp.ecoenchants.enchantments.ecoenchants.artifact;
+
+import com.willfp.eco.core.Prerequisite;
+import com.willfp.ecoenchants.enchantments.itemtypes.Artifact;
+import org.bukkit.Particle;
+import org.jetbrains.annotations.NotNull;
+
+public class SkulkArtifact extends Artifact {
+    public SkulkArtifact() {
+        super(
+                "skulk_artifact",
+                Prerequisite.v1_17
+        );
+    }
+
+    @Override
+    public @NotNull Particle getParticle() {
+        return Particle.VIBRATION;
+    }
+}
---
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/ecoenchants/artifact/SporeArtifact.java
@@ -0,0 +1,20 @@
+package com.willfp.ecoenchants.enchantments.ecoenchants.artifact;
+
+import com.willfp.eco.core.Prerequisite;
+import com.willfp.ecoenchants.enchantments.itemtypes.Artifact;
+import org.bukkit.Particle;
+import org.jetbrains.annotations.NotNull;
+
+public class SporeArtifact extends Artifact {
+    public SporeArtifact() {
+        super(
+                "spore_artifact",
+                Prerequisite.v1_17
+        );
+    }
+
+    @Override
+    public @NotNull Particle getParticle() {
+        return Particle.FALLING_SPORE_BLOSSOM;
+    }
+}
