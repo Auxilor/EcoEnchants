31f07097532bdbd1615bf8155adf9f42889e4f40
Auxilor <william.favierparsons1@gmail.com>
2021-08-04 19:07:47 +0100

    Updated criticals desc and fixed formatting bug

--- eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/EcoEnchant.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/EcoEnchant.java
@@ -1,7 +1,6 @@
 package com.willfp.ecoenchants.enchantments;
 
 import com.willfp.eco.core.Prerequisite;
-import com.willfp.eco.util.StringUtils;
 import com.willfp.ecoenchants.EcoEnchantsPlugin;
 import com.willfp.ecoenchants.config.configs.EnchantmentConfig;
 import com.willfp.ecoenchants.enchantments.meta.EnchantmentRarity;
@@ -207,9 +206,8 @@ public abstract class EcoEnchant extends Enchantment implements Listener, Watche
         availableFromLoot = config.getBool(EcoEnchants.OBTAINING_LOCATION + "loot");
         maxLevel = config.getInt(EcoEnchants.GENERAL_LOCATION + "maximum-level", 1);
         displayName = config.getString("name");
-        description = config.getString("description", false);
-        description = description.replace("&r", this.getPlugin().getLangYml().getString("description-color") + "&r");
-        description = StringUtils.format(description);
+        description = config.getString("description");
+        description = description.replace("§r", "§r" + this.getPlugin().getLangYml().getString("description-color"));
         disabledWorldNames.clear();
         disabledWorldNames.addAll(config.getStrings(EcoEnchants.GENERAL_LOCATION + "disabled-in-worlds"));
         disabledWorlds.clear();
