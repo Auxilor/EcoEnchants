361fbf89bfecc57ab7e4f14815109102fc35d054
Akiranya <porridgen@gmail.com>
2021-09-25 21:56:13 +0800

    Fixed potential memory leak

---
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/util/WeakMetadata.java
@@ -0,0 +1,9 @@
+package com.willfp.ecoenchants.enchantments.util;
+
+import org.bukkit.entity.Entity;
+
+import java.util.WeakHashMap;
+
+public class WeakMetadata {
+    public final static WeakHashMap<Entity, Object> ECO_TARGET = new WeakHashMap<>();
+}
