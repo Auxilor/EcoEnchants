7645e71a6b5304a545bc3200ee2a25f744c5c004
Auxilor <william.favierparsons1@gmail.com>
2021-10-11 09:50:44 +0100

    Updated tornado to improve anticheat compatibility and updated to 8.13.5

--- eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/ecoenchants/normal/Tornado.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/ecoenchants/normal/Tornado.java
@@ -1,9 +1,11 @@
 package com.willfp.ecoenchants.enchantments.ecoenchants.normal;
 
+import com.willfp.eco.core.integrations.anticheat.AnticheatManager;
 import com.willfp.ecoenchants.enchantments.EcoEnchant;
 import com.willfp.ecoenchants.enchantments.EcoEnchants;
 import com.willfp.ecoenchants.enchantments.meta.EnchantmentType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
@@ -24,6 +26,18 @@ public class Tornado extends EcoEnchant {
         double yVelocity = baseVelocity * level;
 
         Vector toAdd = new Vector(0, yVelocity, 0);
+
+        if (victim instanceof Player pVictim) {
+            AnticheatManager.exemptPlayer(pVictim);
+
+            this.getPlugin().getScheduler().runLater(() -> {
+                AnticheatManager.unexemptPlayer(pVictim);
+            }, 40);
+        }
+
+        this.getPlugin().getScheduler().run(() -> {
+            victim.setVelocity(victim.getVelocity().clone().add(toAdd));
+        });
 
         this.getPlugin().getScheduler().runLater(() -> victim.setVelocity(victim.getVelocity().clone().add(toAdd)), 1);
     }
