017e596471713839cbfc2b8ab810da8359c286f0
_OfTeN_ <dkoften@gmail.com>
2022-01-17 23:40:39 +0300

    Fixed Intellect and Wisdom sometimes giving more exp than they should

--- eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/ecoenchants/normal/Wisdom.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/enchantments/ecoenchants/normal/Wisdom.java
@@ -9,7 +9,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wisdom extends EcoEnchant {
+
+    private static final List<Player> toPrevent = new ArrayList<>();
+
     public Wisdom() {
         super(
                 "wisdom", EnchantmentType.NORMAL
@@ -24,7 +30,13 @@ public class Wisdom extends EcoEnchant {
             return;
         }
 
-        if (!EnchantChecks.mainhand(player, this)) {
+        if (!this.areRequirementsMet(player)) {
+            return;
+        }
+
+        int level = EnchantChecks.getMainhandLevel(player, this);
+
+        if (level == 0) {
             return;
         }
 
@@ -32,8 +44,18 @@ public class Wisdom extends EcoEnchant {
             return;
         }
 
-        int level = EnchantChecks.getMainhandLevel(player, this);
+        int newValue = toPrevent.contains(player) ? event.getExpChangeEvent().getAmount() :
+                (int) Math.ceil(event.getExpChangeEvent().getAmount() *
+                        (1 + (level * this.getConfig().getDouble(EcoEnchants.CONFIG_LOCATION + "bonus-per-point"))));
 
-        event.getExpChangeEvent().setAmount((int) Math.ceil(event.getExpChangeEvent().getAmount() * (1 + (level * this.getConfig().getDouble(EcoEnchants.CONFIG_LOCATION + "bonus-per-point")))));
+        event.getExpChangeEvent().setAmount(newValue);
+
+        if (newValue > player.getExpToLevel()) {
+            if (!toPrevent.contains(player)){
+                toPrevent.add(player);
+            }
+        } else {
+            toPrevent.remove(player);
+        }
     }
 }
