83fbad75af838f096fa0400725386c04699fae43
Auxilor <william.favierparsons1@gmail.com>
2022-03-16 20:53:20 +0000

    Updated MythicMobs

--- eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/integrations/mythicmobs/plugins/IntegrationMythicMobs.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoenchants/integrations/mythicmobs/plugins/IntegrationMythicMobs.java
@@ -1,19 +1,19 @@
 package com.willfp.ecoenchants.integrations.mythicmobs.plugins;
 
 import com.willfp.ecoenchants.integrations.mythicmobs.MythicMobsWrapper;
-import io.lumine.xikage.mythicmobs.MythicMobs;
+import io.lumine.mythic.bukkit.MythicBukkit;
 import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
 
 public class IntegrationMythicMobs implements MythicMobsWrapper {
     @Override
     public boolean canDropItems(@NotNull final Entity entity) {
-        if (!MythicMobs.inst().getAPIHelper().isMythicMob(entity)) {
+        if (!MythicBukkit.inst().getAPIHelper().isMythicMob(entity)) {
             return true;
         }
 
-        return !MythicMobs.inst().getAPIHelper().getMythicMobInstance(entity).getType().getPreventMobKillDrops()
-                && !MythicMobs.inst().getAPIHelper().getMythicMobInstance(entity).getType().getPreventOtherDrops();
+        return !MythicBukkit.inst().getAPIHelper().getMythicMobInstance(entity).getType().getPreventMobKillDrops()
+                && !MythicBukkit.inst().getAPIHelper().getMythicMobInstance(entity).getType().getPreventOtherDrops();
     }
 
     @Override
