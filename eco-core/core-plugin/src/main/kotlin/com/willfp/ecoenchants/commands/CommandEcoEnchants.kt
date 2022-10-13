40b6d92a0392d0ecc0b22ef348526373bd81dd7b
Auxilor <william.favierparsons1@gmail.com>
2022-10-13 20:13:39 +0100

    libreforge-updater

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/commands/CommandEcoEnchants.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/commands/CommandEcoEnchants.kt
@@ -2,9 +2,14 @@ package com.willfp.ecoenchants.commands
 
 import com.willfp.eco.core.command.impl.PluginCommand
 import com.willfp.ecoenchants.EcoEnchantsPlugin
+import com.willfp.ecoenchants.enchants.EcoEnchants
+import com.willfp.libreforge.lrcdb.CommandExport
+import com.willfp.libreforge.lrcdb.CommandImport
+import com.willfp.libreforge.lrcdb.ExportableConfig
 import org.bukkit.command.CommandSender
 
-class CommandEcoEnchants(plugin: EcoEnchantsPlugin) : PluginCommand(plugin, "ecoenchants", "ecoenchants.command.ecoenchants", false) {
+class CommandEcoEnchants(plugin: EcoEnchantsPlugin) :
+    PluginCommand(plugin, "ecoenchants", "ecoenchants.command.ecoenchants", false) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
         sender.sendMessage(
             plugin.langYml.getMessage("invalid-command")
@@ -16,5 +21,14 @@ class CommandEcoEnchants(plugin: EcoEnchantsPlugin) : PluginCommand(plugin, "eco
             .addSubcommand(CommandToggleDescriptions(plugin))
             .addSubcommand(CommandGiveRandomBook(plugin))
             .addSubcommand(CommandGUI(plugin))
+            .addSubcommand(CommandImport("enchants", plugin))
+            .addSubcommand(CommandExport(plugin) {
+                EcoEnchants.values().map {
+                    ExportableConfig(
+                        it.id,
+                        it.config
+                    )
+                }
+            })
     }
 }
