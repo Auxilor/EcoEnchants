fdc3bdb236fb913217351f8ed0ab54c7b5ef77ca
Auxilor <william.favierparsons1@gmail.com>
2023-03-18 15:06:02 +0000

    Fixed libreforge v4 bugs

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     compileOnly(project(":eco-core:core-proxy"))
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
     compileOnly("net.essentialsx:EssentialsX:2.19.7")
+    compileOnly("commons-lang:commons-lang:2.6")
 }
 
 tasks {
