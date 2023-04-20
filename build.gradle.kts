b594647855fc1c21f90fed8e80836d62e8148897
Auxilor <william.favierparsons1@gmail.com>
2023-04-20 19:32:42 +0100

    Updated to eco 6.55.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ allprojects {
     }
 
     dependencies {
-        compileOnly("com.willfp:eco:6.53.0")
+        compileOnly("com.willfp:eco:6.55.0")
         compileOnly("org.jetbrains:annotations:23.0.0")
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
         compileOnly("com.github.ben-manes.caffeine:caffeine:3.1.0")
