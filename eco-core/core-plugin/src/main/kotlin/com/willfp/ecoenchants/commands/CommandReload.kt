3eddfd3968c725ef5d6eb06b169b72d97dbafb9f
Will FP <william.favierparsons1@gmail.com>
2023-12-12 12:15:02 +0000

    Fixes

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/commands/CommandReload.kt
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandSender
 
 class CommandReload(plugin: EcoPlugin) : Subcommand(plugin, "reload", "ecoenchants.command.reload", false) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
+        plugin.reload()
         if (Prerequisite.HAS_1_20_3.isMet) {
             sender.sendMessage(plugin.langYml.getMessage("reloaded-1203"))
         } else {
