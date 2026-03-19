cc452d9a2f028c10afe5c56ee54f77d6f6cd3c70
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-19 13:06:23 +0000

    update reload command to display loaded enchant count

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/commands/CommandReload.kt
@@ -3,6 +3,7 @@ package com.willfp.ecoenchants.commands
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
+import com.willfp.ecoenchants.enchant.EcoEnchants
 import com.willfp.ecoenchants.plugin
 import org.bukkit.command.CommandSender
 
@@ -16,6 +17,7 @@ object CommandReload : Subcommand(
         sender.sendMessage(
             plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
                 .replace("%time%", plugin.reloadWithTime().toNiceString())
+                .replace("%count%", EcoEnchants.values().size.toString())
         )
     }
 }
