3bec9bf3d0bc10b47fdeda7065be527105f4a887
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-04-04 23:02:59 +0100

    Update artifactId in publishing configuration to use root project name

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -18,23 +18,11 @@ tasks {
     }
 }
 
-
 publishing {
     publications {
         create<MavenPublication>("shadow") {
             from(components["java"])
-            artifactId = "EcoEnchants"
-        }
-    }
-
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Auxilor/eco")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
+            artifactId = rootProject.name
         }
     }
 
